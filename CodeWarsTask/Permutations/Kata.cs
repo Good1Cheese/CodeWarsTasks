cc74f1d38d7fa575c71e34fcca8ff6052f4052c4
GuterKase <2916675@gmail.com>
2022-10-15 14:08:24 +0300

    Solved "Permutations"

---
+++ CodeWarsTask/Permutations/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWarsTask.Permutations;
+
+public class Kata
+{
+    public static List<string> SinglePermutations(string str)
+    {
+        IEnumerable<string> result = new List<string>() { string.Empty };
+
+        foreach (var _ in str)
+        {
+            result = (from s in str
+                      from r in result
+                      select $"{r}{s}").ToList();
+        }
+
+        result = result.Where(s => HasSameLetters(s, str));
+
+        return result.Distinct().ToList();
+    }
+
+    private static bool HasSameLetters(string s1, string s2)
+    {
+        var arr1 = s1.OrderBy(c => c);
+        var arr2 = s2.OrderBy(c => c);
+
+        return arr1.SequenceEqual(arr2);
+    }
+}
