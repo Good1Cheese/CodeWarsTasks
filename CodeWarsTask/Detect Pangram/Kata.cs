dae086541ef71740b6f89abd1f08bc3ace641911
GuterKase <2916675@gmail.com>
2022-10-20 18:46:57 +0300

    Solved "Detect Pangram"

---
+++ CodeWarsTask/Detect Pangram/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace CodeWarsTask.Detect_Pangram;
+
+public static class Kata
+{
+    private const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static bool IsPangram(string input)
+    {
+        input = input.ToUpper();
+
+        return ALPHABET.All(c => input.Contains(c));
+    }
+}
