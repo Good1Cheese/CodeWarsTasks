7f9b9531804b86b3c27d013f6a6c392f7819e90a
GuterKase <2916675@gmail.com>
2022-10-22 18:23:17 +0300

    Solved "Reverse words"

---
+++ CodeWarsTask/Reverse words/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace CodeWarsTask.Reverse_words;
+
+public static class Kata
+{
+    public static string ReverseWords(string str)
+    {
+        var splited = str.Split();
+
+        var result = string.Join(string.Empty, splited.Select(c => string.Concat(c.Reverse())));
+
+        return result;
+    }
+}
