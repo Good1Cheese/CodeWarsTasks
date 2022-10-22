e93fff09615bcfcd79da4e29173077b33086d18a
GuterKase <2916675@gmail.com>
2022-10-22 18:57:11 +0300

    Solved "Find the capitals"

---
+++ CodeWarsTask/Find the capitals/Kata.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace CodeWarsTask.Find_the_capitals;
+
+public static class Kata
+{
+    public static int[] Capitals(string word)
+    {
+        List<int> result = new();
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            char c = word[i];
+
+            if (char.IsUpper(c))
+            {
+                result.Add(i);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
