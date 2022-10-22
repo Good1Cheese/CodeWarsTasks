9607597819a3851252d6f51ade8df484f6da15dd
GuterKase <2916675@gmail.com>
2022-10-22 18:46:52 +0300

    Edited "Find the next perfect square!"
    Now solution is correct

--- CodeWarsTask/Find the next perfect square!/Kata.cs
+++ CodeWarsTask/Find the next perfect square!/Kata.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using static System.Console;
 
 namespace CodeWarsTask.Find_the_next_perfect_square_;
 
@@ -7,14 +6,12 @@ public class Kata
 {
     public static long FindNextSquare(long number)
     {
-        double squaredNumber = Math.Sqrt(number);
+        long sqrt = (long)Math.Sqrt(number);
 
-        bool isPerfectSquare = squaredNumber % 1 == 0;
+        if (sqrt * sqrt != number) { return -1; }
 
-        if (number <= 0 || !isPerfectSquare) { return -1; }
+        var next = sqrt + 1;
 
-        int smthSmart = 3 + 2 * ((int)squaredNumber - 1);
-
-        return number + smthSmart;
+        return next * next;
     }
 }
