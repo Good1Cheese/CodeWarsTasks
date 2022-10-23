ed47a28cabd6df0fcc4059d8c1781e1594d97587
GuterKase <2916675@gmail.com>
2022-10-23 09:47:38 +0300

    Solved "Summing a number's digits"

---
+++ CodeWarsTask/Summing a number's digits/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace CodeWarsTask.Summing_a_number_s_digits;
+
+public class Kata
+{
+    public static int SumDigits(int number)
+    {
+        var digits = from c in number.ToString()
+                     where char.IsDigit(c)
+                     let s = c.ToString()
+                     select int.Parse(s);
+
+        return digits.Sum();
+    }
+}
