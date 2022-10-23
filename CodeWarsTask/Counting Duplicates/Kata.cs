e0e6703099cf7e7a56040d82aaec0e9a3705e2cd
GuterKase <2916675@gmail.com>
2022-10-23 17:48:59 +0300

    Solved "Counting Duplicates"

---
+++ CodeWarsTask/Counting Duplicates/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace CodeWarsTask.Counting_Duplicates;
+
+public static class Kata
+{
+    private static void Main()
+    {
+        System.Console.WriteLine(DuplicateCount("Indivisibility"));
+    }
+
+    public static int DuplicateCount(string str)
+    {
+        var duplicates = str.GroupBy(c => char.ToLower(c)).Where(g => g.Count() > 1);
+
+        return duplicates.Count();
+    }
+}
