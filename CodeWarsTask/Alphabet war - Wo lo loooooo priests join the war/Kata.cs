fb0cb324c2e5ad95bb5c45c3949e77ee749d7636
GuterKase <2916675@gmail.com>
2022-10-27 21:41:51 +0300

    Solved "Alphabet war - Wo lo loooooo priests join the war"

--- CodeWarsTask/Alphabet war - Wo lo loooooo priests join the war/Kata.cs
+++ CodeWarsTask/Alphabet war - Wo lo loooooo priests join the war/Kata.cs
@@ -1,87 +1,79 @@
-﻿using System.Text;
+﻿using System.Linq;
+using System.Text;
 
-namespace CodeWarsKata.Alphabet_war___Wo_lo_loooooo_priests_join_the_war
+namespace CodeWarsKata.Alphabet_war___Wo_lo_loooooo_priests_join_the_war;
+
+public class Kata
 {
-    public class Kata
+    private const string LEFT_SIDE_LETTERS = "TSBPW";
+    private const string RIGHT_SIDE_LETTERS = "JZDQM";
+
+    private static string _fight;
+    private static StringBuilder _stringBuilderFigth;
+    private static int _score;
+
+    public static string AlphabetWar(string fight)
     {
-        private const string RIGHT_SIDE_WON = "Right side wins!";
-        private const string LEFT_SIDE_WON = "Left side wins!";
-        private const string DRAW = "Let's fight again!";
+        _fight = fight.ToUpper();
+        _stringBuilderFigth = new(_fight);
 
-        public static string AlphabetWar(string fight)
+        _fight = HandlePriestLetters('T');
+        _fight = HandlePriestLetters('J');
+
+        _score += GetSideScore(LEFT_SIDE_LETTERS);
+        _score -= GetSideScore(RIGHT_SIDE_LETTERS);
+
+        return GetWinner();
+    }
+
+    private static string HandlePriestLetters(char priest)
+    {
+        for (int i = 0; i < _fight.Length; i++)
         {
-            int warScore = 0;
+            char c = _fight[i];
 
-            int lastPriestUnitIndex = -1;
-            for (int i = 0; i < fight.Length; i++)
+            if (c == priest)
             {
-                (int, string) currentLetterPower = GetLetterPower(fight[i]);
-                if (currentLetterPower.Item1 == 0 && lastPriestUnitIndex < i)
-                {
-                    fight = AppyPriestUnitPower(fight, i, currentLetterPower.Item2);
-                    lastPriestUnitIndex = i;
-                    warScore = 0;
-                    i = -1;
-                    continue;
-                }
-
-                if (currentLetterPower.Item1 == -1) { continue; }
-                if (currentLetterPower.Item2 == RIGHT_SIDE_LETTERS)
-                {
-                    warScore -= currentLetterPower.Item1;
-                    continue;
-                }
-                warScore += currentLetterPower.Item1;
+                ReplaceAdjacent(i - 1);
+                ReplaceAdjacent(i + 1);
             }
-
-            if (warScore > 0) { return LEFT_SIDE_WON; }
-            if (warScore < 0) { return RIGHT_SIDE_WON; }
-
-            return DRAW;
         }
 
-        private const string LEFT_SIDE_LETTERS = "tsbpw";
-        private const string RIGHT_SIDE_LETTERS = "jzdqm";
+        return _stringBuilderFigth.ToString();
+    }
 
-        private static (int, string) GetLetterPower(char letter)
+    private static void ReplaceAdjacent(int i)
+    {
+        if (i < 0 || i >= _fight.Length) { return; }
+
+        string friends = LEFT_SIDE_LETTERS.Contains(_fight[i]) ? LEFT_SIDE_LETTERS : RIGHT_SIDE_LETTERS;
+        string hostiles = LEFT_SIDE_LETTERS.Contains(_fight[i]) ? RIGHT_SIDE_LETTERS : LEFT_SIDE_LETTERS;
+
+        int power = friends.IndexOf(_fight[i]);
+
+        _stringBuilderFigth[i] = hostiles[power];
+    }
+
+    private static int GetSideScore(string side)
+    {
+        var score = from f in _fight
+                    let i = side.IndexOf(f)
+                    where i != -1
+                    select i;
+
+        return score.Sum();
+    }
+
+    private static string GetWinner()
+    {
+        switch (_score)
         {
-            string side = LEFT_SIDE_LETTERS;
-            int letterPower = LEFT_SIDE_LETTERS.IndexOf(letter);
-
-            if (letterPower == -1)
-            {
-                side = RIGHT_SIDE_LETTERS;
-                letterPower = RIGHT_SIDE_LETTERS.IndexOf(letter);
-            }
-
-            return (letterPower, side);
-        }
-
-        private static string AppyPriestUnitPower(string fight, int unitIndex, string preisetletterSide)
-        {
-            var sb = new StringBuilder(fight);
-
-            if (unitIndex - 1 >= 0)
-            {
-                NewMethod(unitIndex - 1, sb, preisetletterSide);
-            }
-
-            if (unitIndex + 1 <= fight.Length - 1)
-            {
-                if (unitIndex + 1 + 1 >= 0) { if (GetLetterPower(sb[unitIndex]).Item1 == 0) { return sb.ToString(); } }
-                NewMethod(unitIndex + 1, sb, preisetletterSide);
-            }
-
-            return sb.ToString();
-        }
-
-        private static void NewMethod(int unitIndex, StringBuilder sb, string preisetletterSide)
-        {
-            (int, string) currentLetterPower = GetLetterPower(sb[unitIndex]);
-
-            if (preisetletterSide == currentLetterPower.Item2 || currentLetterPower.Item1 == 0 || currentLetterPower.Item1 == -1) { return; }
-            string anotherSource = (currentLetterPower.Item2 == RIGHT_SIDE_LETTERS) ? LEFT_SIDE_LETTERS : RIGHT_SIDE_LETTERS;
-            sb[unitIndex] = anotherSource[currentLetterPower.Item1];
+            case > 0:
+                return "Left side wins!";
+            case < 0:
+                return "Right side wins!";
+            case 0:
+                return "Let's fight again!";
         }
     }
 }
--- CodeWarsTask/Counting Duplicates/Kata.cs
+++ CodeWarsTask/Counting Duplicates/Kata.cs
@@ -4,11 +4,6 @@ namespace CodeWarsTask.Counting_Duplicates;
 
 public static class Kata
 {
-    private static void Main()
-    {
-        System.Console.WriteLine(DuplicateCount("Indivisibility"));
-    }
-
     public static int DuplicateCount(string str)
     {
         var duplicates = str.GroupBy(c => char.ToLower(c)).Where(g => g.Count() > 1);
