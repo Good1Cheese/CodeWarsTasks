fc77e61865798b05f2dda4c2cdbd01366bbc9367
GuterKase <2916675@gmail.com>
2022-10-29 14:24:47 +0300

    Solved "Rock Paper Scissors!"

---
+++ CodeWarsTask/Rock Paper Scissors!/Kata.cs
@@ -0,0 +1,44 @@
+﻿
+namespace CodeWarsTask.Rock_Paper_Scissors_;
+
+public class Kata
+{
+    private static void Main()
+    {
+        System.Console.WriteLine(new Kata().Rps("paper", "rock"));
+    }
+
+    public string Rps(string p1, string p2)
+    {
+        if (p1 == p2) return "Draw!";
+
+        if (p1 == "rock")
+        {
+            return p2 switch
+            {
+                "scissors" => "Player 1 won!",
+                _ => "Player 2 won!"
+            };
+        }
+
+        if (p1 == "scissors")
+        {
+            return p2 switch
+            {
+                "paper" => "Player 1 won!",
+                _ => "Player 2 won!"
+            };
+        }
+
+        if (p1 == "paper")
+        {
+            return p2 switch
+            {
+                "rock" => "Player 1 won!",
+                _ => "Player 2 won!"
+            };
+        }
+
+        return string.Empty;
+    }
+}
