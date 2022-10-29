4b15c16786faf1a45d9dd8bac72702af8be605df
GuterKase <2916675@gmail.com>
2022-10-29 13:55:15 +0300

    Solved "Sort Numbers"

---
+++ CodeWarsTask/Sort Numbers/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWarsTask.Sort_Numbers;
+
+public class Kata
+{
+    public static int[] SortNumbers(int[] nums)
+    {
+        if (nums == null) return Array.Empty<int>();
+
+        return nums.OrderBy(n => n).ToArray();
+    }
+}
