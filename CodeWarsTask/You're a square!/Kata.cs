36c122b5228b2bf85a84d549612ddf7de939d898
GuterKase <2916675@gmail.com>
2022-10-29 13:50:04 +0300

    Solved "You're a square!"

---
+++ CodeWarsTask/You're a square!/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodeWarsTask.You_re_a_square_;
+
+public class Kata
+{
+    public static bool IsSquare(int n)
+    {
+        int sqrt = (int)MathF.Sqrt(n);
+
+        return sqrt * sqrt == n;
+    }
+}
