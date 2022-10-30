9ecbbca3416f29171714559ba05df80defad66c4
GuterKase <2916675@gmail.com>
2022-10-30 21:54:18 +0300

    Solved "Is the string uppercase?"

---
+++ CodeWarsTask/Is the string uppercase/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace CodeWarsTask.Is_the_string_uppercase;
+
+public static class StringExtensions
+{
+    public static bool IsUpperCase(this string text)
+    {
+        var strings = text.Split(' ');
+
+        return strings.All(s =>
+        {
+            return s.All(c => char.IsUpper(c) || !char.IsLetter(c));
+        });
+    }
+}
