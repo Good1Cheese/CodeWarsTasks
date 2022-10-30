fc8edecf167434ed5f7aaf115d42e012c076389f
GuterKase <2916675@gmail.com>
2022-10-30 21:40:47 +0300

    Solved "Basic Mathematical Operations"

---
+++ CodeWarsTask/Basic Mathematical Operations/Kata.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeWarsTask.Basic_Mathematical_Operations;
+
+public static class Kata
+{
+    public static double basicOp(char operation, double v1, double v2)
+    {
+        return operation switch
+        {
+            '+' => v1 + v2,
+            '-' => v1 - v2,
+            '*' => v1 * v2,
+            '/' => v1 / v2,
+            _ => 0
+        };
+    }
+}
