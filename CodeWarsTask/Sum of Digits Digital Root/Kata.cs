25000df8605a9605b1ad7ad0991633c1e2487518
GuterKase <2916675@gmail.com>
2022-11-07 19:26:15 +0300

    Solved "Sum of Digits / Digital Root"

---
+++ CodeWarsTask/Sum of Digits Digital Root/Kata.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace CodeWarsTask.Sum_of_Digits_Digital_Root;
+
+public class Kata
+{
+    public static int DigitalRoot(long n)
+    {
+        while (n >= 10)
+        {
+            var nums = from c in n.ToString()
+                       select int.Parse(c.ToString());
+
+            n = nums.Sum();
+        }
+
+        return (int)n;
+    }
+}
