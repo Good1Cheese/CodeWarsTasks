c6dcca63432939ec18ce646d51463ad3c1dbe1ab
GuterKase <2916675@gmail.com>
2022-11-07 19:08:25 +0300

    Solved "Count of positives / sum of negatives"

---
+++ CodeWarsTask/Count of positives sum of negatives/Kata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWarsTask.Count_of_positives_sum_of_negatives;
+
+public class Kata
+{
+    public static int[] CountPositivesSumNegatives(int[] input)
+    {
+        if (input == null || input.Length == 0) 
+        {
+            return Array.Empty<int>();
+        }
+
+        int positive = input.Where(n => n > 0).Sum();
+        int negative = input.Where(n => n < 0).Sum();
+
+        return new int[] { positive, negative };
+    }
+}
