2092772e5ef1ae0cb909ee1220d5c5d5b1ec239c
GuterKase <2916675@gmail.com>
2022-11-08 20:28:34 +0300

    Solved "Bit Counting"

---
+++ CodeWarsTask/Bit Counting/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWarsTask.Bit_Counting;
+
+public class Kata
+{
+    public static int CountBits(int n)
+    {
+        return Convert.ToString(n, 2).Count(c => c == '1');
+    }
+}
